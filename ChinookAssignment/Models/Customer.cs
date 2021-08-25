4e6c04f4453beea3e17ca9ab3b57ff0d5f1370e0
maaby200 <maaby200@gmail.com>
2021-08-25 12:26:05 +0200

    Removed data that was not used within the Customer poco class. And added summary.

--- ChinookAssignment/Models/Customer.cs
+++ ChinookAssignment/Models/Customer.cs
@@ -6,19 +6,17 @@ using System.Threading.Tasks;
 
 namespace ChinookAssignment
 {
+    /// <summary>
+    /// A class that contains information about a specific customer. POCO class. 
+    /// </summary>
     public class Customer
     {
         public string CustomerID { get; set; }
         public string CustomerFirstName { get; set; }
         public string CustomerSurename { get; set; }
-        public string CompanyName { get; set; }
-        public string Address { get; set; }
-        public string City { get; set; }
-        public string State { get; set; }
         public string Country { get; set; }
         public string PostalCode { get; set; }
         public string Phone{ get; set; }
-        public string Fax { get; set; }
         public string Email { get; set; }
 
     }
--- ChinookAssignment/Models/CustomerCountry.cs
+++ ChinookAssignment/Models/CustomerCountry.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace ChinookAssignment.Models
 {
+    /// <summary>
+    /// A class that contains information about how many customers exist in the database for each existing country. POCO class. 
+    /// </summary>
     public class CustomerCountry
     {
         public string Country { get; set; }
