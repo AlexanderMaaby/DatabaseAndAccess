3ef1116eab1ea8b1972d9143b3428dc4f3036c34
Mia Kristiansen <mia.kristiansen@no.experis.com>
2021-08-26 13:25:20 +0200

    checking if it pushed to right master

--- ChinookAssignment/Program.cs
+++ ChinookAssignment/Program.cs
@@ -18,6 +18,8 @@ namespace ChinookAssignment
             List<CustomerSpender> listetrist = repository.GetAllCustomersTotalAmountSpent();
             CustomerGenre gorg = repository.GetCustomerMostPopularGenre(12);
 
+            ///
+
         }
     }
 }
