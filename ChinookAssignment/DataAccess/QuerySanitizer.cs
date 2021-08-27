c5883b0d096833ad4e7c2136b8749c73aa9b268b
maaby200 <maaby200@gmail.com>
2021-08-27 08:38:31 +0200

    Adding summary for the QuerySanitizer and moved it to the correct folder.

---
+++ ChinookAssignment/DataAccess/QuerySanitizer.cs
@@ -0,0 +1,23 @@
+﻿using System.Data.SqlClient;
+
+namespace ChinookAssignment
+{
+    /// <summary>
+    /// Class to help with getting data from the SqlDataReader in a safe way, while keeping the CustomerRepository clean.
+    /// </summary>
+    public static class QuerySanitizer
+    {
+        /// <summary>
+        /// Get a string from a specific column in the SqlDataReader, or an empty string if it happens to be null.
+        /// </summary>
+        /// <param name="reader">The instance of SqlDataReader that contains the data.</param>
+        /// <param name="colIndex">The index that should be retrieved.</param>
+        /// <returns>The string from the index, or an empty string if the index returns null.</returns>
+        public static string SafeGetString(this SqlDataReader reader, int colIndex)
+        {
+            if (!reader.IsDBNull(colIndex))
+                return reader.GetString(colIndex);
+            return string.Empty;
+        }
+    }
+}
